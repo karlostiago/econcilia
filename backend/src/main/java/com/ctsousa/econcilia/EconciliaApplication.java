cea144f055b47a2b09fdb522d82bf2816fb0b5c2
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-05-25 10:36:31 -0300

    primeiro commit com estrutura inicial

---
+++ backend/src/main/java/com/ctsousa/econcilia/EconciliaApplication.java
@@ -0,0 +1,13 @@
+package com.ctsousa.econcilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconciliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconciliaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ctsousa/econcilia/EconciliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctsousa.econcilia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconciliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
