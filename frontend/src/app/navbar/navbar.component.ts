43f05bd632b9b6791eba87fd6cb3fceb27ae5c3c
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-05-29 14:35:51 -0300

    implementacao do submenu de navegacao

--- frontend/src/app/navbar/navbar.component.ts
+++ frontend/src/app/navbar/navbar.component.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
-    public exibirMenuLateral = false;
+    exibirMenuLateral = false;
+
+    titulo = "";
+
+    ngOnInit(): void {
+        this.titulo = "/Tela Inicial";
+    }
+
+    atualizarTitulo (tituloNovo: string, e: Event) {
+        e.defaultPrevented;
+        this.titulo = "/Tela Inicial"
+
+        if (tituloNovo.length !== 0) {
+            this.titulo = `/Tela de ${this.retornaPrimeiraLetraMaiusculo(tituloNovo).concat(this.retornaPalavraSemPrimeiraLetra(tituloNovo))}`;
+        }
+    }
+
+    private retornaPrimeiraLetraMaiusculo(str: string) {
+        return str.charAt(0).toUpperCase();
+    }
+
+    private retornaPalavraSemPrimeiraLetra(str: string) {
+        return str.substring(1, str.length);
+    }
 }
