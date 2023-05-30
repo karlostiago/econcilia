e40dcc4abc356c6effd48ce6c3e26349dc74435b
Carlos Tiago <carlostiagodesousa@gmail.com>
2023-05-30 17:54:21 -0300

    criando variavies de ambiente para diferenciar ambiente de desenvolvimento com o de producao

--- frontend/src/app/features/operadora/operadora-cadastro/operadora-cadastro.component.ts
+++ frontend/src/app/features/operadora/operadora-cadastro/operadora-cadastro.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-operadora-cadastro',
@@ -8,6 +9,10 @@ import {Component, OnInit} from '@angular/core';
 export class OperadoraCadastroComponent implements OnInit {
 
     taxas: any;
+
+    constructor() {
+        // environment.apiUrl
+    }
 
     ngOnInit(): void {
         this.taxas = [
